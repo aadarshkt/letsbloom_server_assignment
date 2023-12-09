1d55a6c6dc440778849a51785a5e8d950af443b2
aadarshkt <aadarshkt1729@gmail.com>
2023-12-09 15:40:25 +0530

    handle update non-existent book error

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -17,6 +17,13 @@ const updateBook = async (req, res) => {
   const { title, author, year_published, ISBN } = req.body;
   const update_query = "UPDATE books SET title = ?, author = ?, year_published = ?, ISBN = ? WHERE id = ?";
   const result = await query(update_query, [title, author, year_published, ISBN, bookId]);
+  //handling error if updating non-existent book
+  if (result.affectedRows == 0) {
+    res.status(400).json({
+      message: "Invalid book ID",
+    });
+    return;
+  }
   res.json(result);
 };
 
