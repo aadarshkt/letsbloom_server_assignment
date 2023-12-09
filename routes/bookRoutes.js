168912896c335fac52e26a8ba4175d96beb2666b
aadarshkt <aadarshkt1729@gmail.com>
2023-12-09 17:17:25 +0530

    create request validation implemented

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -8,18 +8,33 @@ bookRouter.get("/", (req, res) => {
   getAllbooks(req, res);
 });
 
-bookRouter.post("/", (req, res) => {
-  insertBook(req, res);
-});
-
 //adding validation such that id query parameter is not empty
 //escape helps to prevent XSS -> cross-site scripting vulnerability
 
-const validate_ID = () => query("id").notEmpty().escape();
-const validate_title = () => body("title").notEmpty().escape();
-const validate_author = () => body("author").notEmpty().escape();
-const validate_year_published = () => body("year_published").notEmpty().escape();
-const validate_ISBN = () => body("ISBN").notEmpty().isISBN();
+const validate_ID = () => query("id").trim().notEmpty().escape().withMessage("Id not valid");
+const validate_title = () => body("title").trim().notEmpty().escape().withMessage("title not valid");
+const validate_author = () => body("author").trim().notEmpty().escape().withMessage("author not valid");
+const validate_year_published = () =>
+  body("year_published").trim().notEmpty().escape().withMessage("year published not valid");
+const validate_ISBN = () => body("ISBN").trim().notEmpty().isISBN().withMessage("ISBN not valid");
+
+bookRouter.post(
+  "/",
+  validate_ID(),
+  validate_title(),
+  validate_author(),
+  validate_year_published(),
+  validate_ISBN(),
+  (req, res) => {
+    insertBook(req, res);
+  }
+);
+
+// isDate(options?: {
+//   format?: string;
+//   delimiters?: string[];
+//   strictMode?: boolean;
+// }): ValidationChain
 
 bookRouter.put(
   "/",
