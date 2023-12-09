b2b8099ca4ef7b1bba4e9175e661881580cf7d3e
aadarshkt <aadarshkt1729@gmail.com>
2023-12-09 11:48:43 +0530

    mysql database connection complete

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 import express from "express";
+import cors from "cors";
+import { createConnection } from "mysql2";
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//create database connection
+const connection = createConnection({
+  host: "localhost",
+  user: "root",
+  database: "library_database",
+});
+
+// Connect to the MySQL server
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database: " + err.stack);
+    return;
+  }
+  console.log("Connected to the database as ID " + connection.threadId);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
